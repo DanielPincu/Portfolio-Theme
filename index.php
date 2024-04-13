3e37bea94d9be45204c5e022016d0553c8b0a2eb
DanielPincu <ghepin01@easv365.dk>
2024-04-13 16:18:49 +0200

    Updates

--- index.php
+++ index.php
@@ -270,15 +270,15 @@
       <!-- services -->
       <div class="xl:grid md:grid-cols-4 grid-cols-2 items-center gap-5 md:pb-10 mb-16 hidden">
         <div class="crd-blue crd-red cursor-pointer border-2 h-56 border-blue-300 dark:border-red-300 bg-blue-200 dark:bg-red-200 rounded-3xl drop-shadow-xl flex flex-col items-center mb-10 hover:scale-[103%] duration-500">
-          <h1 class="text-2xl text-center  pt-10">Web Dev</h1>
-          <svg class="w-32 md:w-64 justify-center" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <h1 class="text-2xl text-center text-blue-500 dark:text-red-500 pt-10">Web Dev</h1>
+          <svg class="text-blue-400 dark:text-red-400 w-32 md:w-64 justify-center" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25" />
           </svg>
     
         </div>
         <div class="crd-blue crd-red cursor-pointer border-2 border-blue-300 dark:border-red-300 bg-blue-200 dark:bg-red-200 rounded-3xl drop-shadow-xl h-56 flex flex-col items-center mb-10 hover:scale-[103%] duration-500">
-          <h1 class="text-2xl text-center pt-10">Graphic</h1>
-          <svg class="w-32 md:w-64 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <h1 class="text-2xl text-center text-blue-500 dark:text-red-500 pt-10">Graphic</h1>
+          <svg class="text-blue-400 dark:text-red-400 w-32 md:w-64 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
           </svg>
           
@@ -286,15 +286,15 @@
         
 
         <div class="crd-blue crd-red cursor-pointer border-2 border-blue-300 dark:border-red-300 bg-blue-200 dark:bg-red-200 rounded-3xl drop-shadow-xl h-56 flex flex-col items-center mb-10 hover:scale-[103%] duration-500">
-          <h1 class="text-2xl text-center pt-10">Video edit</h1>
-          <svg class="w-32 md:w-64" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <h1 class="text-2xl text-center text-blue-500 dark:text-red-500 pt-10">Video edit</h1>
+          <svg class="text-blue-400 dark:text-red-400 w-32 md:w-64" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
           </svg>
     
         </div>
         <div class="crd-blue crd-red cursor-pointer border-2 border-blue-300 dark:border-red-300 bg-blue-200 dark:bg-red-200 rounded-3xl drop-shadow-xl h-56 flex flex-col items-center mb-10 hover:scale-[103%] duration-500">
-          <h1 class="text-2xl text-center pt-10">UI / UX</h1>
-          <svg class="w-32 md:w-64" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <h1 class="text-2xl text-center text-blue-500 dark:text-red-500 pt-10">UI / UX</h1>
+          <svg class="text-blue-400 dark:text-red-400 w-32 md:w-64" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
           </svg>
           
@@ -335,20 +335,31 @@
 
 
     <!-- about me -->
+
     <div class="border-b-2 border-zinc-200 pb-20" id="about">
-    <div class="shadow-xl rounded-3xl mt-20 px-5 2xl:px-14 pb-20 bg-blue-200 dark:bg-red-200 border-2 border-blue-300 dark:border-red-300 crd-blue crd-red" data-aos="flip-right" data-aos-duration="1000">
+    <div class="shadow-xl rounded-3xl mt-20 px-5 2xl:px-14 pb-20 bg-blue-200 dark:bg-red-200 border-2 border-blue-300 dark:border-red-300 crd-blue crd-red">
       <div>
         <div class="flex justify-center">
           <h1 class="text-4xl py-10">About Me</h1>
         </div>
-        <div class="grid xl:grid-cols-2 gap-10 grid-cols-1">
-          <div>
-            <p v-for="(paragraph, index) in paragraphs" :key="index" class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minima labore eligendi veniam, nesciunt voluptatibus vitae pariatur amet, ratione illo, excepturi iusto consequatur nihil quas sit hic odit deleniti provident?</p>
-          </div>
-          <div class="hidden lg:block">
-            <img class="rounded-3xl" src="imageSrc" alt="">
-          </div>
-        </div>
+
+                  <?php
+                    $about_loop = new WP_Query(array(
+                        "post_type" => "about",
+                        "posts_per_page" => -1
+                    ));
+                    ?>
+                    <?php if($about_loop->have_posts()): ?>
+                    
+                        <?php while($about_loop->have_posts()): $about_loop->the_post() ?>
+
+                            <?php get_template_part("template-parts/about"); ?>
+                           
+
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata() ?>
+                  <?php endif; ?> 
+      
 
       </div>
     </div>
---
+++ template-parts/about.php
@@ -0,0 +1,14 @@
+<?php
+$about_me = get_field("about_me");
+$me = get_field("me")['url'];
+?>
+         <div class="grid xl:grid-cols-2 gap-10 grid-cols-1">
+        
+        <div>
+            <p class="mb-5"><?php echo $about_me ?></p>
+        </div>
+
+          <div class="hidden lg:block">
+            <img class="rounded-3xl" src="<?php echo $me ?>" alt="">
+          </div>
+        </div>
